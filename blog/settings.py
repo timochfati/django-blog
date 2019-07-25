7931d5d3efc763becc5dca3c18ab8974767c691a
timochfati <lamrini788@gmail.com>
2019-07-25 11:44:31 +0000

    made changes in settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-forms-bootstrap',
+    'django_forms_bootstrap',
     'posts',
 ]
 
